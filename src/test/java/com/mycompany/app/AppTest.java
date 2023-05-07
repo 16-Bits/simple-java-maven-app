d912902185a3686aeca26036cb2136bf71a96b5a
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2023-05-06 21:53:54 -0700

    fixed ignore?

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Ignore;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
